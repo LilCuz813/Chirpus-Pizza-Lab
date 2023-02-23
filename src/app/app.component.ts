64b499fae847881f66f866156f1a86cb8bd08d8e
chris <chrisabelw@m3.com>
2023-02-23 15:44:25 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Menu';
+  title = 'Chirpus Pizza';
+
+  allItems:MenuItem[] = [
+    {
+      itemName: "12 pc Wing Combo",
+      itemType: "Chicken",
+      itemPrice: 8.99,
+      itemRating: 8,
+      recommend: true
+    },
+    {
+      itemName: "1 lb Catfish Nugget Special",
+      itemType: "Fish",
+      itemPrice: 7.99,
+      itemRating: 10,
+      recommend: true
+    },
+    {
+      itemName: "12 slice Deep Dish Pizza",
+      itemType: "Pizza",
+      itemPrice: 6.99,
+      itemRating: 8,
+      recommend: false
+    },
+    {
+      itemName: "24 slice Specialty Pizza ",
+      itemType: "Pizza",
+      itemPrice: 12.99,
+      itemRating: 10,
+      recommend: true
+    },
+    {
+      itemName: "Garden Salad",
+      itemType: "Veggie",
+      itemPrice: 0.00,
+      itemRating: 2,
+      recommend: false
+    },
+    {
+      itemName: "Fruit Salad",
+      itemType: "Veggie",
+      itemPrice: 0.00,
+      itemRating: 10,
+      recommend: true
+    },
+  ]
 }
+
+
---
+++ src/app/menu-item.ts
@@ -0,0 +1,7 @@
+export interface MenuItem {
+    itemName:string;
+    itemType:string;
+    itemRating:number;
+    itemPrice:number;
+    recommend:boolean;
+}
